393fa44c698ab2fc5db4de7d220267b5917c492f
Gaston Chiesa <chiesajosegaston@gmail.com>
2023-08-29 01:09:32 -0300

    modificando estilos y animaciones para despliegue de menu carrito + armado de primera parte de logica del carrito

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
-  fetchData();
-});
+//**conexion con los elementos del DOM - INICIO:
 
-//conexion a base de datos de productos: "data.json"
+const $cardsContainer = document.querySelector(".product-cards-container"); //contenedor general de cards
+const $cardItem = document.querySelectorAll(".product-card-item"); // contenedor de cada card
+const $cardBtn = document.querySelectorAll(".btn-card"); //botones de card
+
+const $cartContainer = document.querySelector(".cart"); //capturo contenedor del carrito
+const $cartIcon = document.querySelector(".bxs-cart"); //capturo icono del carrito
+const $cartDropdown = document.querySelector(".cart-dropdown"); //despliege del menu del carrito
+
+//*conexion con los elementos del DOM - FINAL
+
+let cartShop = {};
+
+//---------------------------------------------------
+
+//? ---- CONTENEDOR CARDS INICIO ----
+
+//*conexion a base de datos de productos: "data.json" - INICIO
 const fetchData = async () => {
   try {
     const res = await fetch("/assets/json/data.json");
@@ -13,10 +27,7 @@ const fetchData = async () => {
     console.log(error);
   }
 };
-
-//conexion con los elementos del DOM:
-const cardsContainer = document.querySelector(".product-cards-container"); //contenedor de cards
-console.log(cardsContainer);
+//*conexion a base de datos de productos: "data.json" - FINAL
 
 const templateCardProduct = (product) => {
   return `
@@ -38,7 +49,13 @@ const templateCardProduct = (product) => {
               <p class="card-info-buys-listPrice">$ ${product.precioLista}</p>
             </div>
             <div class="card-info-buys-btn">
-              <button class="btn-card button">Comprar</button>
+              <button class="btn-card button" data-id="${
+                product.id
+              }" data-nombre="${product.nombre}" data-precio="${(
+    product.precioLista * product.descuento
+  )
+    .toFixed(2)
+    .replace(".", ",")}" data-img="${product.imagen_02}" >Comprar</button>
             </div>
           </div>
         </div>
@@ -46,11 +63,75 @@ const templateCardProduct = (product) => {
 };
 
 const renderCardProducts = (data) => {
-  return (cardsContainer.innerHTML = data
+  $cardsContainer.innerHTML = data
     .map((product) => templateCardProduct(product))
-    .join(""));
+    .join("");
 };
 
-const init = () => {
-  cardsContainer.addEventListener("DOMContentLoaded", renderCardProducts);
+//?---- CONTENEDOR CARDS FINAL ----
+
+//*---- CONTENEDOR CARRITO INICIO ----
+
+const toggleCart = () => {
+  //para desplegar y ocultar menu
+  $cartDropdown.classList.toggle("cart-dropdown");
+  $cartDropdown.classList.toggle("cart-show");
 };
+
+const addCart = (e) => {
+  //para agregar producto al carrito
+
+  if (e.target.classList.contains("btn-card")) {
+    setCart(e.target.parentElement.parentElement.parentElement);
+  }
+  e.stopPropagation();
+};
+
+const setCart = (cart) => {
+  console.log(cart);
+  const product = {
+    id: cart.querySelector(".btn-card").dataset.id,
+    name: cart.querySelector(".btn-card").dataset.nombre,
+    price: cart.querySelector(".btn-card").dataset.precio,
+    img: cart.querySelector(".btn-card").dataset.img,
+    quantity: 1,
+  };
+  if (cartShop.hasOwnProperty(product.id)) {
+    product.quantity = cartShop[product.id].quantity + 1;
+  }
+  cartShop[product.id] = { ...product };
+  console.log(product);
+};
+
+//   return ($cartDropdown.innerHTML = `
+//   <h3 class="cart-title">Productos Cargados a tu Carrito:</h3>
+//   <p class="no-products">No hay productos cargados en tu Carrito</p>
+//   <div class="cart-body">
+//     <img class="cart-img" src="${product.imagen_02}" alt="${product.nombre}">
+//     <p class="cart-product-name">Producto: ${product.nombre}</p>
+//     <p class="cart-product-price">Precio: $${(
+//       product.precioLista * product.descuento
+//     )
+//       .toFixed(2)
+//       .replace(".", ",")}</p>
+//     <div class="cart-quantity-container">
+//       <p class="cart-product-quantity">Cantidad: 1</p>
+//       <button class="cart-product-btn-add">+</button>
+//       <button class="cart-product-btn-rem">-</button>
+//     </div>
+//     <p class="cart-product-subtotal">Subtotal: $300 </p>
+//   </div>
+//   <p class="cart-product-total">Total</p>
+//   `);
+// };
+
+//*---- CONTENEDOR CARRITO FINAL ----
+
+const init = () => {
+  document.addEventListener("DOMContentLoaded", fetchData); //para renderizar apenas termine de cargar el browser
+
+  $cartIcon.addEventListener("click", toggleCart); //para desplegar carrito
+
+  $cardsContainer.addEventListener("click", addCart); //para agregar productos al carrito
+};
+init();
